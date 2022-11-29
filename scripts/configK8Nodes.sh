4ed4d754b02dc464e5c3e76a1aa747dce030ed94
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2022-11-29 02:32:54 -0500

    Update configK8Nodes.sh

--- scripts/configK8Nodes.sh
+++ scripts/configK8Nodes.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 4 ]] ; then
-	numMasterNodes="$4"
-	echo "Number of Master nodes = $numMasterNodes"
-elif [[ $# -eq 3 ]] ; then
-	numMasterNodes=1
-	echo "Number of Master nodes = $numMasterNodes"
-else
-	echo "Expected at least 3 CLI arguments - Interface name, Number of worker nodes and total num of VMs to configure"
+if [ "$#" -ne 1 ]; then
+	echo "Expected at least 1 CLI argument - Interface name"
     exit 1
 fi
 
 intf="$1"
-numWorkerNodes="$2"
-numTotalNodes="$3"
+masterNode="02"
+declare -a workerNodes=("08" "09" "10" "11" "12" "14" "16" "17" "18")
+declare -a ranNodes=("21" "27")
 
 #https://computingforgeeks.com/deploy-kubernetes-cluster-on-ubuntu-with-kubeadm/
 #https://computingforgeeks.com/install-mirantis-cri-dockerd-as-docker-engine-shim-for-kubernetes/
@@ -27,10 +22,10 @@ echo ""
 echo "Configuring Master Node"
 echo ""
 
-mcmd="bash /opt/Secure5G/scripts/configMasterNode.sh $intf"
-eval $mcmd
-kjoincmdorig=$(kubeadm token create --print-join-command)
-kjoincmd="${kjoincmdorig}"
+mcmd="cd /opt/Secure5G/ && git pull && bash /opt/Secure5G/scripts/configMasterNode.sh $intf"
+sshpass -p $MYPASSWD ssh -o StrictHostKeyChecking=no root@cap$masterNode "$mcmd"
+kjoincmdorig="kubeadm token create --print-join-command"
+kjoincmd=$(sshpass -p $MYPASSWD ssh -o StrictHostKeyChecking=no root@cap$masterNode "$kjoincmdorig")
 
 echo ""
 echo "Finished Configuring Master Node. Sleep for 120 seconds..."
@@ -43,21 +38,17 @@ echo "Configuring Worker Nodes with command - $kjoincmd"
 echo ""
 
 
-wcmd="cd /opt/Secure5G/scripts/ && git pull && bash /opt/Secure5G/scripts/configWorkerNode.sh \"$kjoincmd\" && exit"
-nodeNum=$((0 + numMasterNodes))
-startNodeNum=$((0 + numMasterNodes))
-endNodeNum=$((numWorkerNodes + numMasterNodes - 1))
-for i in $(seq $startNodeNum $endNodeNum);
+wcmd="cd /opt/Secure5G/ && git pull && bash /opt/Secure5G/scripts/configWorkerNode.sh \"$kjoincmd\" && exit"
+for nodeNum in "${workerNodes[@]}"
 do	
-	node=node$nodeNum
+	node=cap$nodeNum
 	echo ""
 	echo "Configuring Node - $node"
 	echo ""
-	ssh -o StrictHostKeyChecking=no root@$node "$wcmd"
+	sshpass -p $MYPASSWD ssh -o StrictHostKeyChecking=no root@$node "$wcmd"
 	echo ""
 	echo "Finished Configuring Worker Node - $node"
-        echo ""
-        nodeNum=$((nodeNum + 1))
+    echo ""
 done
 
 echo ""
@@ -68,21 +59,17 @@ echo ""
 echo "Started Configuring RAN Nodes"
 echo ""
 
-rcmd="cd /opt/Secure5G/scripts/ && git pull && bash /opt/Secure5G/scripts/configRanNode.sh && exit"
-nodeNum=$(($numWorkerNodes + $numMasterNodes))
-startNodeNum=$(($numWorkerNodes + $numMasterNodes))
-endNodeNum=$((numTotalNodes - 1))
-for i in $(seq $startNodeNum $endNodeNum);
+rcmd="cd /opt/Secure5G/ && git pull && bash /opt/Secure5G/scripts/configRanNode.sh && exit"
+for nodeNum in "${ranNodes[@]}"
 do	
-	node=node$nodeNum
+	node=cap$nodeNum
 	echo ""
 	echo "Configuring Node - $node"
 	echo ""
-	ssh -o StrictHostKeyChecking=no root@$node "$rcmd"
+	sshpass -p $MYPASSWD ssh -o StrictHostKeyChecking=no root@$node "$rcmd"
 	echo ""
 	echo "Finished Configuring RAN Node - $node"
-        echo ""
-        nodeNum=$((nodeNum + 1))
+    echo ""
 done
 
 echo ""
