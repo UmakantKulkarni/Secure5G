bceae13999e260f01cc1e14298dd60f9c25e4d5b
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2022-11-29 15:19:02 -0500

    Update labelK8sNodes.sh

--- scripts/labelK8sNodes.sh
+++ scripts/labelK8sNodes.sh
@@ -3,14 +3,16 @@
 declare -a nodeLabels=("master" "amf" "smf" "upf" "udsf" "ausf" "nrf" "pcf" "udm")
 declare -a workerNodes=("02" "08" "09" "11" "12" "14" "16" "17" "18")
 
+arrayIndex=0
 for nodeNum in "${workerNodes[@]}"
 do	
 	node=cap$nodeNum
 	echo ""
 	echo "Labelling Node - $node"
 	echo ""
-    kubectl label --overwrite nodes $node kubernetes.io/pcs-nf-type=${nodeLabels[i]}
+    kubectl label --overwrite nodes $node kubernetes.io/pcs-nf-type=${nodeLabels[arrayIndex]}
 	echo ""
 	echo "Finished Labelling Node - $node"
     echo ""
+    arrayIndex=$((arrayIndex + 1))
 done
