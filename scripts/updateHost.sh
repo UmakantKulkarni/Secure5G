f9ce8b84bfbd1fc03413981e8681a74f43bcaed1
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2023-02-06 20:31:43 -0500

    Create updateHost.sh

---
+++ scripts/updateHost.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mydir=/opt
+
+cd $mydir
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+
+cd $mydir
+wget https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.7.5.2/aptsources-cleanup.pyz
+chmod a+x aptsources-cleanup.pyz
+all | ./aptsources-cleanup.pyz -y
+
+apt-get -y update && apt-get -y upgrade && apt-get -y update && apt-get -y dist-upgrade
